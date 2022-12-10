bee28502d032870c8a50abdd5e3428b7527cbb1b
Petro Sobchuk <sobchuk.p@gmail.com>
2022-12-10 16:03:23 +0200

    11/04-creating-deleting

---
+++ 11/js/04-creating-deleting.js
@@ -0,0 +1,52 @@
+/**
+  |============================
+  | Створення
+  |============================
+*/
+const heading = document.createElement("h1");
+console.log("heading: ", heading); // <h1></h1>
+
+heading.textContent = "This is a heading";
+console.log("heading: ", heading); // <h1>This is a heading</h1>
+
+const image = document.createElement("img");
+image.src = "https://placeimg.com/640/480/nature";
+image.alt = "Nature";
+console.log("image: ", image); // <img src="https://placeimg.com/640/480/nature" alt="Nature" />
+
+/**
+  |============================
+  | Додавання
+  |============================
+*/
+
+const list = document.querySelector(".usernames");
+
+// Adds an item to the end of the list
+const lastItem = document.createElement("li");
+lastItem.textContent = "Poly";
+list.append(lastItem);
+
+// Adds an item to the beginning of the list
+const firstItem = document.createElement("li");
+firstItem.textContent = "Ajax";
+list.prepend(firstItem);
+
+// Adds a title before the list
+const title = document.createElement("h2");
+title.textContent = "USERNAMES";
+list.before(title);
+
+// Adds a paragraph after the list
+const text = document.createElement("p");
+text.textContent =
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum tenetur assumenda fugiat maxime, soluta aspernatur quasi nihil in asperiores ad distinctio illo et debitis error iure voluptate numquam maiores nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum tenetur assumenda fugiat maxime, soluta aspernatur quasi nihil in asperiores ad distinctio illo et debitis error iure voluptate numquam maiores nisi!";
+list.after(text);
+
+/**
+  |============================
+  | Видалення
+  |============================
+*/
+const text2 = document.querySelector(".text");
+text2.remove();
